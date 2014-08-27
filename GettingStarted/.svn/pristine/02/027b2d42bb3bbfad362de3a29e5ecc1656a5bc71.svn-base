86df820f9d9c6adc65efa6fbe891bb1c6f67c8fe
ly <986417576@qq.com>
2014-08-27 09:32:17 +0800

    Revert "a map project"
    This reverts commit 9e21ddfee0e79ed15e7feb715acab151521fb85f.

--- GettingStarted/.svn/pristine/02/027b2d42bb3bbfad362de3a29e5ecc1656a5bc71.svn-base
+++
@@ -1,6 +0,0 @@
-package com.lilo.model;
-
-public class MessageEnum {
-	public static final int QUERY_SUCCESS = 0;
-	public static final int QUERY_FAILED = 1;
-}
--- GettingStarted/gen/com/lilo/sm/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.lilo.sm;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
